66c9d77dc5afa50549ba13886018acfddd385e4a
xiao-lix <lixiao900921@gmail.com>
2020-08-28 13:56:13 -0700

    update angular package name

---
+++ extensions/applicationinsights-angularplugin-js/src/applicationinsights-angularplugin-js.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import '@microsoft/applicationinsights-shims';
+import AngularPlugin from "./AngularPlugin";
+import { IAngularExtensionConfig } from "./Interfaces/IAngularExtensionConfig";
+
+export { AngularPlugin, IAngularExtensionConfig };
